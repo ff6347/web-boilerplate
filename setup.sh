adf3d41fa5eeeed736b9494fbbadeaa0bd6e229b
fabiantheblind <icke@fabianmoronzirfas.me>
2014-01-30 14:58:13 +0100

    some more changes

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-git init
-echo "Hello World" > README.md
-echo ".DS_Store" > .gitignore
-echo "*.sublime-workspace" >> .gitignore
-echo "*.sublime-project" >> .gitignore
-echo "node_modules/" >> .gitignore
-echo "node_modules/" >> .gitignore
-echo "bower_components/" >> .gitignore
+#git init
+#echo "Hello World" > README.md
+#echo ".DS_Store" > .gitignore
+#echo "*.sublime-workspace" >> .gitignore
+#echo "*.sublime-project" >> .gitignore
+#echo "node_modules/" >> .gitignore
+#echo "node_modules/" >> .gitignore
+#echo "bower_components/" >> .gitignore
 npm install
 bower install
 # npm install grunt grunt-contrib-watch grunt-contrib-connect load-grunt-tasks grunt-contrib-copy grunt-bower-task grunt-bower-install grunt-text-replace  grunt-contrib-stylus  grunt-contrib-jade  connect-livereload grunt-contrib-coffee grunt-open --save-dev
-git add -A
-git commit -a -m"initial local commit"
+#git add -A
+#git commit -a -m"initial local commit"
 grunt copy-bower
 grunt install-bower
 grunt
